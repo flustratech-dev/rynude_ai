31348c5fb0bee443aa3a620d30a9afb6a8a18fec
ryantro <mryanrizki11@gmail.com>
2026-06-21 13:10:37 +0700

    fix(server): suppress broken pipe notice on SSE streaming disconnects

---
+++ server.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$publicPath = getcwd();
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists($publicPath.$uri)) {
+    return false;
+}
+
+$formattedDateTime = date('D M j H:i:s Y');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$remoteAddress = $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'];
+
+// Use @ to suppress broken pipe notices which occur frequently 
+// when using SSE streaming responses and the client disconnects.
+@file_put_contents('php://stdout', "[$formattedDateTime] $remoteAddress [$requestMethod] URI: $uri\n");
+
+require_once $publicPath.'/index.php';
