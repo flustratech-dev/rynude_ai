596a73b3c8a6faae3de85a17198f58131ba0c6e2
ryantro <mryanrizki11@gmail.com>
2026-06-21 13:23:19 +0700

    fix(server): resolve public directory path correctly for manual php -S execution

--- server.php
+++ server.php
@@ -7,7 +7,7 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-$publicPath = getcwd();
+$publicPath = __DIR__.'/public';
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
